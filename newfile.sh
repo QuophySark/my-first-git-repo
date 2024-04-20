0e94b3d8951f415e9084628c3ea1884b27fd40b8
QuophySark <kofisark25@gmail.com>
2024-04-20 00:54:19 -0400

    added few changes

--- newfile.sh
+++ newfile.sh
@@ -3,4 +3,6 @@
 # This is a text file to test our first Github repository
 #
 # Hello, I'm here to tell you that am the original owner of this file.
+#
+# ITYOURWAY.tech is the best IT boot camp you can learn DevOps Engineering.
 
